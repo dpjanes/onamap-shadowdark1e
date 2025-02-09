c93e48b2f7dae5c4118ec673c7a8be44d6e55c31
David Janes <davidjanes@gmail.com>
2025-02-09 04:16:26 -0500

    Add attacks to various monster YAML files

--- rulebooks/InProgress/monsters/process.py
+++ rulebooks/InProgress/monsters/process.py
@@ -1,4 +1,4 @@
-import yaml
+import yaml # type: ignore
 import re
 import os
 import string
@@ -23,7 +23,7 @@ for monster in monsters:
     level = f"{monster['level']['value']:02}"
     if level != "01":
         continue
-    
+
     os.makedirs(level, exist_ok=True)
 
     monster["id"] = f"monster:Core:{monster_name}:{level}"
@@ -37,7 +37,6 @@ for monster in monsters:
         attack_name = attack["name"] = string.capwords(attack["name"])
         tohit = attack.get("tohit", 0)
         range = attack.get("range", None)
-        print(range)
 
         if attack_name == "Spell":
             monster_bonuses.append({
@@ -49,7 +48,7 @@ for monster in monsters:
 
         if range == "near":
             attack["range"] = "melee"
-            attack["DELETE"] = True
+            # attack["DELETE"] = True
             pass
 
         if tohit:
@@ -69,7 +68,11 @@ for monster in monsters:
     monster = scrub(monster)
 
     with open(f"{level}/{monster_name}.yaml", "w") as fout:
-        monster_ordered = {"name": monster.pop("name"), "description": monster.pop("description")}
+        monster_ordered = {
+            "id": monster.pop("id"), 
+            "name": monster.pop("name"), 
+            "description": monster.pop("description"),
+        }
         monster_ordered.update(monster)
         fout.write(yaml.dump(monster_ordered, sort_keys=False))
         fout.write("\n")
