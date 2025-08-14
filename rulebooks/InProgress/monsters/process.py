c6aa809522eef0c01ec1b9f94c01cd6182924881
David Janes <davidjanes@gmail.com>
2025-08-14 04:20:05 -0400

    spell.cast.add

--- rulebooks/InProgress/monsters/process.py
+++ rulebooks/InProgress/monsters/process.py
@@ -97,7 +97,7 @@ for monster in monsters:
         if attack_name == "Spell":
             attack["types"] = "spell"
             monster_bonuses.append({
-                "key": "spell.cast",
+                "key": "spell.cast.add",
                 "value": tohit
             })
         else:
